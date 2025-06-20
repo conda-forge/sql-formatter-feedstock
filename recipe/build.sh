ddeecf85f3281b5f5807f16e8181cb3d07199500
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-19 17:32:47 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,7 @@ jq 'del(.scripts.prepare)' package.json.bak > package.json
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -17,7 +18,14 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/sql-formatter << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/sql-formatter/bin/sql-formatter-cli.cjs "\$@"
+EOF
+chmod +x ${PREFIX}/bin/sql-formatter
+
 # Create batch wrapper
 tee ${PREFIX}/bin/sql-formatter.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\sql-formatter %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\sql-formatter\bin\sql-formatter-cli.cjs %*
 EOF
